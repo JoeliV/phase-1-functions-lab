a894aab6670037f54b7821b77ee81b1338359b3f
Joeli Vasquez <joeliv106@gmail.com>
2023-01-31 13:54:23 -0500

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+  if(someValue > 42){
+    return someValue - 42;
+  }
+  else {
+    return 42 - someValue;
+  }
+}
+
+function distanceFromHqInFeet(someValue) {
+let numBlocks;
+numBlocks = distanceFromHqInBlocks(someValue)
+let blockFeet = numBlocks *264;
+
+return blockFeet;
+}
+
+
+function distanceTravelledInFeet(start, destination) {
+  let distance;
+  distance = Math.abs(destination - start) 
+  let blockFeet = distance *264;
+return blockFeet;
+  
+}
+
+function calculatesFarePrice(start, destination) {
+
+  let blockFeet = ((Math.abs(start - destination)* 264));
+  if (blockFeet <= 400){
+    return 0;
+  }
+  else if  (blockFeet> 400 && blockFeet <= 2000){
+    return (.02 * (blockFeet-400))
+  }
+  else if (blockFeet >2000 && blockFeet < 2500){
+    return 25;
+  }
+  else{
+    return('cannot travel that far')
+  }
+}
